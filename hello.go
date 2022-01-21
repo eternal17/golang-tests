962042631db87011e6a6694b973a24bf0d1cd2a3
eternal17 <sarmadkhatri15@gmail.com>
2022-01-21 17:30:59 +0000

    adding other languages to the greeting

--- hello.go
+++ hello.go
@@ -4,12 +4,26 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+const spanishHelloPrefix = "Hola, "
+
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	}
+
+	return prefix + name
 }
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
