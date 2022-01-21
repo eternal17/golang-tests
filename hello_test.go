3cbebf9ca54224c94714e2a1f448c883b23bfbd6
eternal17 <sarmadkhatri15@gmail.com>
2022-01-21 16:44:00 +0000

    refactoring different failing tests into a function

--- hello_test.go
+++ hello_test.go
@@ -7,11 +7,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	// If the test fails
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// Helper is needed to tell the test suite that this method is a helper.
+		// By doing this when it fails the line number reported will be in our function call
+		//  rather than outside rather than inside our test helper.
+		// This will help other developers track down problems easier.
+		t.Helper()
+		// if test fails
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
